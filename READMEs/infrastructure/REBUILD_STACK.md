6cbf1b03263a1baa7ef259806e76ee579c5007a5
Austin Smith <austindsmith_accounts@proton.me>
2025-11-25 20:37:16 -0700

    removed old unworking files left in last commit

--- READMEs/infrastructure/REBUILD_STACK.md
+++
@@ -1,4 +0,0 @@
-Literally just run this:
-terraform destroy && terraform apply
-ansible-playbook site.yml
-./scripts/deploy-complete-stack.sh
--- terraform/archive/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "k3s_master_ip" {
-  value = "192.168.50.11"
-}
-
-output "k3s_worker_ips" {
-  value = [for i in range(2) : format("192.168.50.%d", 12 + i)]
-}
